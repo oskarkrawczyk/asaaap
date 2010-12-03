d35e81e6437ba75747ffb24ec109b80e29c43c92
btolarz <btolarz@gmail.com>
2010-12-03 17:54:04 +0100

    initia commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = 'ba54b4c017f903621b73cddd736255fd625c9e0f91e9ce0b0bf629379d97a740ce43b4377fa1f4fc2828e0241eb280ac7ecb30f6f7a8825a84a3a972a45bb4d2'
