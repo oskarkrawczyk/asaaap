686e5ca294927c52b697e99fd11b8945adcd8197
btolarz <btolarz@gmail.com>
2010-12-08 09:40:56 +0100

    fixed hash generator

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,9 +12,9 @@ class ListsController < ApplicationController
   def new
     @list = List.new
     @list.list_items.build
-    @list.list_hash = rand(9999)
-    #@list.list_hash = Digest::MD5.hexdigest({Time.now.to_s, rand}.to_s)[1..5]
-    
+    # @list.list_hash = rand(9999)
+    # @list.list_hash = Digest::MD5.hexdigest({Time.now.to_s, rand}.to_s)[1..5]
+    @list.list_hash = ActiveSupport::SecureRandom.hex(3)
     respond_to do |format|
       if @list.save
         format.html { redirect_to list_path(@list.list_hash) }
