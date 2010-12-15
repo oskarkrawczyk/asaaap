a0dde09fbd854a8c158afe5a0d9242e2244b185b
btolarz <btolarz@gmail.com>
2010-12-15 20:48:21 +0100

    Changed update method

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,9 +11,6 @@ class ListsController < ApplicationController
   
   def new
     @list = List.new
-    @list.list_items.build
-    # @list.list_hash = rand(9999)
-    # @list.list_hash = Digest::MD5.hexdigest({Time.now.to_s, rand}.to_s)[1..5]
     @list.list_hash = ActiveSupport::SecureRandom.hex(3)
     respond_to do |format|
       if @list.save
@@ -25,26 +22,31 @@ class ListsController < ApplicationController
   end
   
   def update
-    
-    @done_tasks = params[:done]
-    @done_tasks.each do |task|
-      
-    end
-    
-    
-    # "done"=>{"0"=>{"title"=>"Helo", "id"=>"7"}, "1"=>{"title"=>"done task", "id"=>"4"}}
-    #    
-    #    "list"=>{"name"=>"asd", "list_items_attributes"=>{"0"=>{"title"=>"Helo", "id"=>"7", "_destroy"=>"0", "status"=>""}}}
-    
     @list = List.find_by_list_hash(params[:id])
+    @list.list_items.destroy_all
     
-    respond_to do |format|
-      if @list.update_attributes(params[:list])
-        #format.html { redirect_to list_path(@list.list_hash) }
-        #format.xml { head :ok }
-        format.json { @list.to_json }
+    params[:list] = {}
+    
+    if params[:done]
+      @done_tasks = params[:done]
+      @done_tasks.each do |k,task|
+        task['status'] = "done"
+        task['id'] = nil
       end
+      params[:list][:list_items_attributes] = @done_tasks
+      @list.update_attributes(params[:list])
     end
+    
+    if params[:active]
+      @active_tasks = params[:active]
+      @active_tasks.each do |k,task|
+        task['status'] = "active"
+        task['id'] = nil
+      end
+      params[:list][:list_items_attributes] = @active_tasks
+      @list.update_attributes(params[:list])
+    end
+    
   end
   
 end
