1a26445b118ddcd1ecdca82421ea36687192fd31
btolarz <btolarz@gmail.com>
2010-12-15 22:47:13 +0100

    fixed response after save

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -46,7 +46,7 @@ class ListsController < ApplicationController
       params[:list][:list_items_attributes] = @active_tasks
       @list.update_attributes(params[:list])
     end
-    
+    render :text => "ok"
   end
   
 end
