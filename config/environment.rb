06ab95e9915ffb721d052e909155027ee10c0a48
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2010-12-27 23:12:00 +0100

    Dreamhost gems hack

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,6 @@
 # Load the rails application
+ENV['GEM_PATH'] = File.expand_path('~/.gems') + ':/usr/lib/ruby/gems/1.8'
+
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
